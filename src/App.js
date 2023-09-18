8468600299d8d628a5979de51b0c74a42389e012
Yuta4u <yudhistira4u@gmail.com>
2023-09-18 16:33:02 +0700

    tailwind daisyui setup

--- src/App.js
+++ src/App.js
@@ -1,25 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react"
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="flex justify-center items-center h-screen bg-gray-200">
+      <div className="bg-white p-8 rounded shadow-lg">
+        <h1 className="text-2xl font-bold mb-4">
+          React App with Tailwind CSS and DaisyUI
+        </h1>
+        <button className="btn btn-xs sm:btn-sm bg-slate-700 text-white">
+          Responsive
+        </button>
+      </div>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
