83981311fb1500e611bae939f7aefb9a030b1abc
Yuta4u <yudhistira4u@gmail.com>
2023-09-25 17:39:52 +0700

    done

--- src/api/api.js
+++ src/api/api.js
@@ -2,7 +2,7 @@ import axios from "axios"
 
 export const getAllUrlPokemon = async () => {
   const res = await axios.get(
-    "https://pokeapi.co/api/v2/pokemon?limit=700&offset=0"
+    "https://pokeapi.co/api/v2/pokemon?limit=649&offset=0"
   )
   return res
 }
